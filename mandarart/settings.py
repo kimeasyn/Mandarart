ba3f8a010e0fe3e0ccb26ad6df1d02ce285b86ff
kimeasyn <kimeasyn@gmail.com>
2017-12-12 21:28:31 +0900

    bootstrap 적용과 layout.html 설정

--- mandarart/settings.py
+++ mandarart/settings.py
@@ -56,7 +56,9 @@ ROOT_URLCONF = 'mandarart.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'mandarart', 'templates'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
