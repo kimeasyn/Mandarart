1988f51f4ec7d93fe5fb5bf8036cb6619506e33f
kimeasyn <kimeasyn@gmail.com>
2017-12-13 23:51:22 +0900

    borad_new 화면에 jquery animation script 작성

--- board/static/board/board_new.js
+++ board/static/board/board_new.js
@@ -1,9 +1,54 @@
 $(document).ready(function(){
     startpage();
+    setEvent();
+    setInputTagChangeEvent();
+
 });
 
-function startpage() {
+function setEvent(){
+
+    $("#delete_button").click(function(){
+        alert("@");
+        $("form").find("input[type=text], textarea").val("");
+    });
+
     $('#sub1_toggle_btn').click(function(){
         $('#sub_input').toggle("slide");
-    })
+    });
+
+}
+
+function setInputTagChangeEvent(){
+
+    setChangeEvent("#id_main_goal", "#main_goal_cell");
+
+    for(var i = 1; i < 9; i++){
+        var source_id = "#id_sub_goal" + i.toString();
+        var target_id = "#sub_goal_cell" + i.toString();
+        setChangeEvent(source_id, target_id);
+    }
+
+}
+function startpage() {
+
+    // $("#id_main_goal").change(function () {
+    //     var str = $(this).val();
+    //     $("#main_goal_cell").text(str);
+    // });
+    //
+    // setChangeEvent("#id_main_goal", "#main_goal_cell");
+    //
+    // for(var i = 1; i < 9; i++){
+    //     var source_id = "#id_sub_goal" + i.toString();
+    //     var target_id = "#sub_goal_cell" + i.toString();
+    //     setChangeEvent(source_id, target_id);
+    // }
+
+}
+
+function setChangeEvent(source_id, target_id){
+    $(source_id).change(function () {
+        var str = $(source_id).val();
+        $(target_id).text(str);
+    });
 }
